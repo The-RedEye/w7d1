540b8e98ae0de2ac055d5bc1cc163996ae3191f9
Tristan Angeles <setswipe@gmail.com>
2021-09-20 14:14:34 -0400

    finsihed user controller, starting sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+
+    # after_initialize: ensure_session_token
+
+    attr_reader :password
 
     def reset_session_token!
         self.session_token = SecureRandom::urlsafe_base64
