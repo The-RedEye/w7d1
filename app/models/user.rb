2003018ccee23028e3c65dd5b1ebd08c9a01856b
spark <spark11491@gmail.com>
2021-09-20 13:48:37 -0400

    finish user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ApplicationRecord
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+
+        temp_password = Bcrypt::Password.new(self.password_digest)
+        temp_password.is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
 end
